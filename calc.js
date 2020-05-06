1d0f63afdb4d1e7283cd0c8a369405492c517dbf
ramsetiawan <setiawanramadyan@gmail.com>
2020-05-06 22:00:43 +0700

    update web storage

--- calc.js
+++ calc.js
@@ -57,8 +57,16 @@ const calculator = {
     } else {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
     }
-  
+    
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+    putHistory(history);
     calculator.displayNumber = result;
+    renderHistory();
  }
   
   
